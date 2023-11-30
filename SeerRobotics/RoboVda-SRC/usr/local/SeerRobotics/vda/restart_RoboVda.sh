7cb6fdc89a802bf53a164ce1cb42058472e8e33c
CXN <3203238028@qq.com>
2023-11-30 18:57:58 +0800

    + RoboVda restart shell

---
+++ SeerRobotics/RoboVda-SRC/usr/local/SeerRobotics/vda/restart_RoboVda.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# 查找 RoboVda 的进程ID（PID）
+PID=$(ps aux | grep RoboVda | grep -v grep | awk '{print $2}')
+
+# 如果存在进程ID，则终止进程
+if [[ ! -z "$PID" ]]; then
+  echo "Terminating RoboVda process (PID: $PID)..."
+  kill $PID
+fi
+
+# 切换到 RoboVda 程序所在目录
+cd /usr/local/SeerRobotics/vda/
+
+# 以后台进程的方式重新启动 RoboVda
+sudo ./RoboVda
+
+echo "RoboVda restarted successfully."
